12b847161b3b337f585e40c3fd1351aa5e1ebba6
unknown <showlinakter77@gmail.com>
2024-04-18 00:59:34 +0600

    making a reusableForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
 
 import './App.css'
-import HoockForm from './Components/HoockForm/HoockForm'
+import ReusableForm from './Components/ReusableForm/ReusableForm'
+// import HoockForm from './Components/HoockForm/HoockForm'
 // import RefForm from './Components/RefForm/RefForm'
 // import StateFullForm from './Components/StateFullForm/StateFullForm'
 // import SimpleForm from './Components/SimpleForm/SimpleForm'
 
 function App() {
-  
+
+  const handleSignUpSubmit = data => {
+   console.log('Sign up data',data);
+  }
+  const handleUpdateProfile = data => {
+    console.log('update profile',data);
+  }
+
   return (
-    <>   
-      <h1>Vite + React</h1>
+    <>
+      <h1>Form Master</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFullForm></StateFullForm> */}
       {/* <RefForm></RefForm> */}
-      <HoockForm></HoockForm>
-
+      {/* <HoockForm></HoockForm> */}
+      <ReusableForm
+        formTitle={'Sign Up'}
+        handleSubmit={handleSignUpSubmit}>
+          <div>
+            <h2>Sign Up</h2>
+            <p>Please Sign up right now</p>
+          </div>
+      </ReusableForm>
+      <ReusableForm
+        formTitle={'Profile Update'}
+        handleSubmit={handleUpdateProfile}
+        submitBtnText='Update'>
+          <div>
+            <h2>Update Profile</h2>
+            <p>Always keep your profile updated</p>
+          </div>
+        </ReusableForm>
 
     </>
   )
