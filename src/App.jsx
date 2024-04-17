934cfe96d067ebce893bfbc0248da92ac45d70fc
unknown <showlinakter77@gmail.com>
2024-04-17 23:11:04 +0600

    create form

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import RefForm from './Components/RefForm/RefForm'
+// import StateFullForm from './Components/StateFullForm/StateFullForm'
+// import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+  
+  return (
+    <>   
+      <h1>Vite + React</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <StateFullForm></StateFullForm> */}
+      <RefForm></RefForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/__form__.js
@@ -0,0 +1,8 @@
+/* 
+ * 1. use form tag and onSubmit handler with event (e)
+ * From a access e.target.[name of the input field].value
+ * 2.controled element:use individual field state for each input field
+ * 3. controled element:one object in a stste holding all the input fields value
+ * 4. uncontrolled element: useRef to create a referance to the element and 
+ * access value by using like:nameRef.current.value
+ */ 
