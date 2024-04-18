b12bb43774c36dedf7673ddc6da69e0099889d33
unknown <showlinakter77@gmail.com>
2024-04-18 13:33:14 +0600

    prips replace createContext add

--- src/Components/GrandPa/GrandPa.jsx
+++ src/Components/GrandPa/GrandPa.jsx
@@ -1,20 +1,39 @@
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './GrandPa.css';
 
+export const AssetContext = createContext('gold');
+export const MoneyContext = createContext(1000);
+
+
 const GrandPa = () => {
-    const asset='diamond';
+    const [money, setMoney] =useState(1000);
+    const asset = 'diamond';
     return (
         <div className="grandpa ">
             <h2>Grandpa</h2>
-            <section className="flex">
-            <Dad asset={asset}></Dad>
-            <Uncle asset={asset}></Uncle>
-            <Aunty></Aunty>
-            </section>
+            <p>Net Money: {money} </p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="gold">
+                    <section className="flex">
+                        <Dad asset={asset}></Dad>
+                        <Uncle asset={asset}></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
 
 export default GrandPa;
+
+/**** 
+ * 1. Create a context and export
+ * 2. add provider for the context with a value
+ * 3. use context to access value in the context API
+ * 
+*/
+
